4ac04ecddaa6d38d7b8dde1d4c000946fc59c7c6
obullxl@gmail.com <obullxl@gmail.com@83e4dedf-1127-58bd-4bb1-5dd734efb98d>
2012-09-14 03:28:33 +0000

    增加权限明细事件体

---
+++ trunk/src/main/java/com/atom/core/event/model/RightDetailEvent.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2012 All Rights Reserved.
+ */
+package com.atom.core.event.model;
+
+import java.io.Serializable;
+
+import com.atom.core.ToString;
+
+/**
+ * 权限明细事件
+ * 
+ * @author shizihu
+ * @version $Id: RightDetailEvent.java, v 0.1 2012-9-14 上午11:28:24 shizihu Exp $
+ */
+public class RightDetailEvent extends ToString implements Serializable {
+    private static final long serialVersionUID = -4568703606727384007L;
+
+}
