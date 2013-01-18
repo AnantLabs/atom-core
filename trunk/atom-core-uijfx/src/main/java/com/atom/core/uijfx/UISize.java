d57b81d8d5b289002e1b454aaab73bbe73f0bf8f
obullxl@gmail.com <obullxl@gmail.com@83e4dedf-1127-58bd-4bb1-5dd734efb98d>
2013-01-18 13:42:47 +0000

    增加用户信息

--- trunk/atom-core-uijfx/src/main/java/com/atom/core/uijfx/UISize.java
+++ trunk/atom-core-uijfx/src/main/java/com/atom/core/uijfx/UISize.java
@@ -14,36 +14,69 @@ import javafx.stage.Screen;
  * @version $Id: UISize.java, 2012-8-18 下午8:25:49 Exp $
  */
 public class UISize {
+    /** 默认尺寸 */
+    public static final int D_WIDTH  = 800;
+    public static final int D_HEIGHT = 600;
 
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
-    private double width;
-    private double height;
+    /** 最佳尺寸 */
+    public static final int P_WIDTH  = 1020;
+    public static final int P_HEIGHT = 750;
 
-    public UISize() {
+    /** 宽 */
+    private double          width;
+    /** 高 */
+    private double          height;
+
+    private UISize() {
     }
 
-    public UISize(double width, double height) {
+    private UISize(double width, double height) {
         this.width = width;
         this.height = height;
     }
 
+    /**
+     * 尺寸
+     */
     public static UISize to(double width, double height) {
         return new UISize(width, height);
     }
 
-    public static UISize toDefault() {
-        return to(WIDTH, HEIGHT);
+    /**
+     * 最佳尺寸
+     */
+    public static UISize toPrefSize() {
+        return to(P_WIDTH, P_HEIGHT);
     }
 
+    /**
+     * 默认尺寸
+     */
+    public static UISize toDefault() {
+        return to(D_WIDTH, D_HEIGHT);
+    }
+
+    /**
+     * 最大尺寸，屏幕大小的80%
+     */
     public static UISize toMaxSize() {
         Rectangle2D screen = Screen.getPrimary().getVisualBounds();
         return to(screen.getWidth() * 0.8, screen.getHeight() * 0.8);
     }
 
+    /**
+     * 屏幕大小
+     */
     public static UISize toScreenSize() {
         Rectangle2D screen = Screen.getPrimary().getVisualBounds();
         return to(screen.getWidth(), screen.getHeight());
+    }
+
+    /** 
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return "[" + this.width + ", " + this.height + "]";
     }
 
     public double getWidth() {
