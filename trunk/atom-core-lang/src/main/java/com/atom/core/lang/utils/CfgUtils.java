0d847642eed0a03d148aa2c44a944bcc0d863bab
obullxl@gmail.com <obullxl@gmail.com@83e4dedf-1127-58bd-4bb1-5dd734efb98d>
2013-01-24 03:44:21 +0000

    增加工具类

--- trunk/atom-core-lang/src/main/java/com/atom/core/lang/utils/CfgUtils.java
+++ trunk/atom-core-lang/src/main/java/com/atom/core/lang/utils/CfgUtils.java
@@ -1,147 +1,103 @@
 /**
  * aBoy.com Inc.
- * Copyright (c) 2004-2011 All Rights Reserved.
+ * Copyright (c) 2004-2012 All Rights Reserved.
  */
 package com.atom.core.lang.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.io.File;
 
-import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
 
 /**
- * 系统配置工具类
+ * 默认配置工具类
  * 
  * @author obullxl@gmail.com
- * @version $Id: SystemConfigUtils.java, 2011-12-24 下午12:02:10 Exp $
+ * @version $Id: CfgUtils.java, 2012-8-18 下午8:25:49 Exp $
  */
-public final class CfgUtils {
+public class CfgUtils {
+
+    /** 根目录 */
+    private static String ROOT_PATH        = "";
+
+    /** 配置目录 */
+    private static String CONFIG_PATH      = "";
+    private static String CONFIG_PATH_NAME = "cfgs";
 
     /**
-     * 系统参数配置
+     * 设置配置目录名
      */
-    private static final Map<String, String> config = new ConcurrentHashMap<String, String>();
+    public static final void setCfgPathName(String cfgPathName) {
+        if (StringUtils.isBlank(cfgPathName)) {
+            throw new RuntimeException("设置系统配置目录名错误！");
+        }
 
-    /**
-     * 设置系统参数
-     */
-    public static final void setPorperty(String key, String value) {
-        Assert.notNull(key, "[系统参数]-设置系统参数-系统参数Key为NULL.");
-        Assert.notNull(value, "[系统参数]-设置系统参数-系统参数Value为NULL.");
-
-        config.put(key, value);
-        System.setProperty(key, value);
+        CONFIG_PATH = "";
+        CONFIG_PATH_NAME = cfgPathName;
     }
 
     /**
-     * 设置系统参数
+     * 设置根目录
      */
-    public static final void setPorperty(Map<String, String> map) {
-        if (!CollectionUtils.isEmpty(map)) {
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
+    public static final void setRootPath(String rootPath) {
+        if (StringUtils.isBlank(rootPath)) {
+            throw new RuntimeException("设置系统根目录错误！");
+        }
 
-                if (StringUtils.isNotBlank(key) && StringUtils.isNotBlank(value)) {
-                    setPorperty(key, value);
+        ROOT_PATH = rootPath;
+    }
+
+    /**
+     * 设置配置目录
+     */
+    public static final void setConfigPath(String configPath) {
+        if (StringUtils.isBlank(configPath)) {
+            throw new RuntimeException("设置系统配置目录错误！");
+        }
+
+        CONFIG_PATH = configPath;
+    }
+
+    /**
+     * 应用系统根目录
+     */
+    public static final String findRootPath() {
+        if (StringUtils.isBlank(ROOT_PATH)) {
+            File root = new File(".");
+            ROOT_PATH = FilenameUtils.normalizeNoEndSeparator(root.getAbsolutePath());
+            LogUtils.info("系统根目录：" + ROOT_PATH);
+        }
+
+        return ROOT_PATH;
+    }
+
+    /**
+     * 应用系统配置目录
+     * <p/>
+     * 查找当前或上一级‘cfgs’目录
+     */
+    public static final String findConfigPath() {
+        if (StringUtils.isBlank(CONFIG_PATH)) {
+            File root = new File(".");
+
+            // ./cfgs
+            File config = new File(root, CONFIG_PATH_NAME);
+            if (config.exists() && config.isDirectory()) {
+                CONFIG_PATH = FilenameUtils.normalizeNoEndSeparator(config.getAbsolutePath());
+                LogUtils.info("系统配置目录：" + CONFIG_PATH);
+            } else {
+                // ../cfgs
+                config = new File(root, "../" + CONFIG_PATH_NAME);
+                if (config.exists() && config.isDirectory()) {
+                    CONFIG_PATH = FilenameUtils.normalizeNoEndSeparator(config.getAbsolutePath());
+                    LogUtils.info("系统配置目录：" + CONFIG_PATH);
+                } else {
+                    throw new RuntimeException("系统配置目录没有找到，请检查系统配置是否正确！");
                 }
             }
         }
+
+        return CONFIG_PATH;
     }
-
-    /**
-     * 检测是否存在参数
-     */
-    public static final boolean exist(String key) {
-        if (StringUtils.isBlank(key)) {
-            return false;
-        }
-
-        boolean exist = config.containsKey(key);
-        if (!exist) {
-            String prop = System.getProperty(key);
-            if (StringUtils.isNotBlank(prop)) {
-                exist = true;
-            }
-        }
-
-        return exist;
-    }
-
-    /**
-     * 获取所有配置信息
-     */
-    public static final Map<String, String> getAll() {
-        Map<String, String> all = new HashMap<String, String>();
-
-        for (Map.Entry<String, String> entry : config.entrySet()) {
-            all.put(entry.getKey(), entry.getValue());
-        }
-
-        return all;
-    }
-
-    /**
-     * 获取系统参数String
-     */
-    public static final String getString(String key) {
-        Assert.notNull(key, "[系统参数]-获取系统参数-系统参数Key为NULL.");
-
-        String value = config.get(key);
-        if (StringUtils.isBlank(value)) {
-            value = System.getProperty(key);
-        }
-
-        return value;
-    }
-
-    /**
-     * 获取系统参数String
-     */
-    public static final String getString(String key, String defaultValue) {
-        String value = getString(key);
-
-        if (StringUtils.isBlank(value)) {
-            value = defaultValue;
-        }
-
-        return value;
-    }
-
-    /**
-     * 获取系统参数Boolean
-     */
-    public static final boolean getBoolean(String key, boolean defaultValue) {
-        String value = getString(key, String.valueOf(defaultValue));
-        return BooleanUtils.toBoolean(value);
-    }
-
-    /**
-     * 获取系统参数Integer
-     */
-    public static final int getInteger(String key, int defaultValue) {
-        try {
-            return Integer.parseInt(getString(key));
-        } catch (Exception e) {
-            return defaultValue;
-        }
-    }
-
-    /**
-     * 获取系统参数Long
-     */
-    public static final long getLong(String key, long defaultValue) {
-        try {
-            return Long.parseLong(getString(key));
-        } catch (Exception e) {
-            return defaultValue;
-        }
-    }
-
-    // ~~~~~~~~~~~ 个性化方法 ~~~~~~~~~~~ //
 
 }
