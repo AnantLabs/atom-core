8cde2c5ecc13243242a961c5a8452796f0fb2a15
obullxl@gmail.com <obullxl@gmail.com@83e4dedf-1127-58bd-4bb1-5dd734efb98d>
2013-01-28 03:04:30 +0000

    增加Prefs功能

---
+++ trunk/atom-core-lang/src/main/java/com/atom/core/lang/prefs/PrefUtils.java
@@ -0,0 +1,65 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2013 All Rights Reserved.
+ */
+package com.atom.core.lang.prefs;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 
+ * @author shizihu
+ * @version $Id: PrefUtils.java, v 0.1 2013-1-28 上午10:16:40 shizihu Exp $
+ */
+public class PrefUtils {
+    private static final ReadWriteLock        _lock = new ReentrantReadWriteLock();
+    private static final Map<String, PrefMap> _map  = new ConcurrentHashMap<String, PrefMap>();
+
+    public static final String get(String catg, String key) {
+        _lock.readLock().lock();
+        try {
+            return ensure(catg).get(key);
+        } finally {
+            _lock.readLock().unlock();
+        }
+    }
+
+    public static final void put(String catg, String key, String value) {
+        _lock.writeLock().lock();
+        try {
+            ensure(catg).put(key, value);
+        } finally {
+            _lock.writeLock().unlock();
+        }
+    }
+
+    public static final String remove(String catg, String key) {
+        _lock.writeLock().lock();
+        try {
+            return ensure(catg).remove(key);
+        } finally {
+            _lock.writeLock().unlock();
+        }
+    }
+
+    private static final PrefMap ensure(String category) {
+        PrefMap pref = _map.get(category);
+        if (pref == null) {
+            _lock.writeLock().lock();
+            try {
+                if (pref == null) {
+                    pref = new PrefMap(category);
+                    _map.put(category, pref);
+                }
+            } finally {
+                _lock.writeLock().unlock();
+            }
+        }
+
+        return pref;
+    }
+
+}
