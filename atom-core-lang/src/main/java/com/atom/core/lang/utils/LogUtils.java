4852e355ca298dd2e3f5fe6b0039652a1cf089fd
obullxl@gmail.com <obullxl@gmail.com@83e4dedf-1127-58bd-4bb1-5dd734efb98d>
2013-01-29 11:49:57 +0000

    完善功能组件

--- atom-core-lang/src/main/java/com/atom/core/lang/utils/LogUtils.java
+++ atom-core-lang/src/main/java/com/atom/core/lang/utils/LogUtils.java
@@ -15,14 +15,57 @@ import java.util.logging.Logger;
  * @version $Id: LogUtils.java, 2012-8-18 下午8:25:49 Exp $
  */
 public class LogUtils {
-    private static final PrintStream out = System.out;
-    private static final PrintStream err = System.err;
-    
-    private static final Logger logger = Logger.getLogger("LogUtils");
+    private static boolean           debugEnable = true;
+    private static boolean           infoEnable  = true;
+
+    private static final PrintStream out         = System.out;
+    private static final PrintStream err         = System.err;
+    private static final Logger      logger      = Logger.getLogger("LOG");
+
+    /**
+     * 设置是否打印Debug级别日志
+     */
+    public static void setDebugEnable(boolean debug) {
+        debugEnable = debug;
+    }
+
+    /**
+     * 设置是否打印Info级别日志
+     */
+    public static void setInfoEnable(boolean info) {
+        infoEnable = info;
+    }
+
+    public static void debug(String msg) {
+        if (debugEnable) {
+            out.println(msg);
+            logger.log(Level.FINER, msg);
+        }
+    }
+
+    public static void debug(String msg, Throwable e) {
+        if (debugEnable) {
+            out.println(msg);
+            e.printStackTrace();
+
+            logger.log(Level.FINER, msg);
+        }
+    }
 
     public static void info(String msg) {
-        out.println(msg);
-        logger.log(Level.INFO, msg);
+        if (infoEnable) {
+            out.println(msg);
+            logger.log(Level.INFO, msg);
+        }
+    }
+    
+    public static void info(String msg, Throwable e) {
+        if (infoEnable) {
+            out.println(msg);
+            e.printStackTrace();
+            
+            logger.log(Level.INFO, msg);
+        }
     }
 
     public static void warn(String msg) {
@@ -37,10 +80,17 @@ public class LogUtils {
         logger.log(Level.WARNING, msg, e);
     }
 
+    public static void error(String msg) {
+        err.println(msg);
+
+        logger.log(Level.SEVERE, msg);
+    }
+    
     public static void error(String msg, Throwable e) {
         err.println(msg);
         e.printStackTrace();
 
         logger.log(Level.SEVERE, msg, e);
     }
+    
 }
